1ceb1fe699c6a9ca26be3ab242209fcc7e3a3040
rachelis234 <rl4157291@gmail.com>
2019-11-27 19:06:47 +0200

    re=acheli first commit

--- client-new/src/app/components/student-home/student-home.component.ts
+++ client-new/src/app/components/student-home/student-home.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
-  selector: 'app-student-home',
-  templateUrl: './student-home.component.html',
-  styleUrls: ['./student-home.component.css']
+  selector: "app-student-home",
+  templateUrl: "./student-home.component.html",
+  styleUrls: ["./student-home.component.css"]
 })
 export class StudentHomeComponent implements OnInit {
+  constructor(private route: Router, private cookieService: CookieService) {}
 
-  constructor(private route:Router) { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  logOut() {
+    this.cookieService.delete("userName");
+    this.cookieService.delete("password");
+    this.route.navigate([""]);
   }
-
 }
